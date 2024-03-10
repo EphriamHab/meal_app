fb5e328ea19b94d5b78207988f2017266d4fd756
EphriamHab <ephremhabtamu2015@gmail.com>
2024-03-11 00:50:56 +0300

    categories

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:meal_app/data/dummy_data.dart';
+import 'package:meal_app/screens/meals.dart';
 import 'package:meal_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+              title: 'some title',
+              meals: [],
+            )));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,16 +23,21 @@ class CategoriesScreen extends StatelessWidget {
       body: GridView(
         padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, 
+          crossAxisCount: 2,
           childAspectRatio: 1.5,
           mainAxisSpacing: 20,
-          crossAxisSpacing: 20, 
+          crossAxisSpacing: 20,
         ),
         children: [
           for (final category in availableCategories)
-            CategoryGridItem(category: category)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context);
+              },
+            )
         ],
-        ),
+      ),
     );
   }
 }
