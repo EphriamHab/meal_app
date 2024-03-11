566a6faf141b8e3bf9b06ffde2f5fd44146d7d2c
EphriamHab <ephremhabtamu2015@gmail.com>
2024-03-11 20:41:11 +0300

    meals

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meal_app/models/meal.dart';
+import 'package:meal_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
@@ -27,9 +28,7 @@ class MealsScreen extends StatelessWidget {
     if(meals.isNotEmpty){
       content =ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (ctx, index)=>Text(
-      meals[index].title,
-      ),);
+        itemBuilder: (ctx, index)=>MealItem(meal: meals[index]));
     }
     return Scaffold(
       appBar: AppBar(
