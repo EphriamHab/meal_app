b80191c90cf976d0093d8255150eb5f737d47aa0
EphriamHab <ephremhabtamu2015@gmail.com>
2024-03-12 00:32:00 +0300

    Meal item

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -4,15 +4,18 @@ import 'package:meal_app/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({super.key, required this.meal});
+  const MealItem({super.key, required this.meal, required this.onSelectMeal});
 
   final Meal meal;
+  final void Function( Meal meal) onSelectMeal;
   String get complexityText {
-    return meal.complexity.name[0].toUpperCase() + meal.complexity.name.substring(1);
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
   }
 
-    String get affordabilityText {
-    return meal.affordability.name[0].toUpperCase() + meal.affordability.name.substring(1);
+  String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
   }
 
   @override
@@ -25,7 +28,9 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          onSelectMeal(meal);
+        },
         child: Stack(
           children: [
             FadeInImage(
@@ -79,7 +84,7 @@ class MealItem extends StatelessWidget {
                         ),
                         MealItemTrait(
                           icon: Icons.attach_money,
-                          label: affordabilityText ,
+                          label: affordabilityText,
                         ),
                       ],
                     )
