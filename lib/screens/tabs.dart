78918b86b13be5f92eea709d955435ba3c419b1f
EphriamHab <ephremhabtamu2015@gmail.com>
2024-03-28 14:45:03 +0300

    tabs

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:meal_app/data/dummy_data.dart';
-import 'package:meal_app/models/meal.dart';
 import 'package:meal_app/providers/favorites_provider.dart';
 import 'package:meal_app/screens/categories.dart';
 import 'package:meal_app/screens/filters.dart';
 import 'package:meal_app/screens/meals.dart';
 import 'package:meal_app/widgets/main_drawer.dart';
 import 'package:meal_app/providers/meals_provider.dart';
+import 'package:meal_app/providers/filters_provider.dart';
 
 // ignore: constant_identifier_names
 const KInitialsFilters = {
@@ -29,14 +28,6 @@ class TabsScreen extends ConsumerStatefulWidget {
 class _TabsScreenState extends ConsumerState<TabsScreen> {
   int _selectedPageIndex = 0;
   Map<Filter, bool> _selectedFilters = KInitialsFilters;
-  void _showInfoMessage(String message) {
-    ScaffoldMessenger.of(context).clearSnackBars();
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      content: Text(message),
-    ));
-  }
-
-
 
   void _selectPage(int index) {
     setState(() {
